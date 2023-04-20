f7ffc4700173c4aca242f2a869d0dd565276494f
Niccies <nicci4elite@yahoo.com>
2023-04-20 08:01:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main -entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0(success)
  */
 int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		return (0);
 }
