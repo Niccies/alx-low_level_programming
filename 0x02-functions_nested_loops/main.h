57b1edfd5b1bac7855884721c260afe53e88b6c6
Niccies <nicci4elite@yahoo.com>
2023-04-25 15:50:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-void print_alphabet(void);
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
 void print_alphabet_x10(void);
-int_putchar(int c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,3 +13,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
+
+
